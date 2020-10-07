7a7a3d375bb0ac215d97d7e236153e364cdbdc4c
Mohanee <mohanee99@gmail.com>
2020-10-07 10:16:08 +0530

    [mohanee] refactor. modified ealier code for both first and last name

--- EmailCheck/Program.cs
+++ EmailCheck/Program.cs
@@ -9,30 +9,36 @@ namespace EmailCheck
         {
             Console.WriteLine("Hello Welcome to User registration.");
             CheckRegex cr = new CheckRegex();
-            string fname = cr.FirstNameCondn();
-            Console.WriteLine(fname);
-
+            string fname = cr.NameCondn(1);
+            Console.WriteLine("Your First Name is :"+fname);
+            string lname = cr.NameCondn(2);
+            Console.WriteLine("Your Last Name is :"+ lname);
         }
     }
 
     class CheckRegex
     {
-        public string FirstNameCondn()
+        public string NameCondn(int n)
         {
             bool validate = true;
-            string firstNamePattern = "[A-Z][A-za-z]{2,}$";
+            string namePattern = "[A-Z][A-za-z]{2,}$";
             string validName = null;
+            string nameType = "First";
+            if(n==2)
+            {
+                nameType = "Last";
+            }
             while (validate)
             {
-                Console.WriteLine("Enter your First Name(First Letter Capital and min 3 alphabets)");
-                string fName = Console.ReadLine();
-                if (!(Regex.IsMatch(fName, firstNamePattern)))
+                Console.WriteLine("Enter your "+nameType+" Name(First Letter Capital and min 3 alphabets)");
+                string name = Console.ReadLine();
+                if (!(Regex.IsMatch(name, namePattern)))
                 {
-                    Console.WriteLine("Please enter a valid first name");
+                    Console.WriteLine("Please enter a valid "+nameType+" name");
                 }
                 else
                 {
-                    validName = fName;
+                    validName = name;
                     validate = false;
                 }
             }
