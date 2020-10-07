faea9017defcdfb7111888195fceeb6d9d81c449
Mohanee <mohanee99@gmail.com>
2020-10-07 10:01:05 +0530

    [mohanee] add first name condition

--- EmailCheck/Program.cs
+++ EmailCheck/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace EmailCheck
 {
@@ -6,7 +7,36 @@ namespace EmailCheck
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Welcome to User registration. This is Master Branch");
+            Console.WriteLine("Hello Welcome to User registration.");
+            CheckRegex cr = new CheckRegex();
+            string fname = cr.FirstNameCondn();
+            Console.WriteLine(fname);
+
+        }
+    }
+
+    class CheckRegex
+    {
+        public string FirstNameCondn()
+        {
+            bool validate = true;
+            string firstNamePattern = "[A-Z][A-za-z]{2,}$";
+            string validName = null;
+            while (validate)
+            {
+                Console.WriteLine("Enter your First Name(First Letter Capital and min 3 alphabets)");
+                string fName = Console.ReadLine();
+                if (!(Regex.IsMatch(fName, firstNamePattern)))
+                {
+                    Console.WriteLine("Please enter a valid first name");
+                }
+                else
+                {
+                    validName = fName;
+                    validate = false;
+                }
+            }
+            return validName;
         }
     }
 }
